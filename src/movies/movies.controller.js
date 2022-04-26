e0ea2ca5b14015e24de34ac321b7dab77b64f71c
ambersweep <applemix5@gmail.com>
2022-04-26 03:58:34 -0400

    completed movies route

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -6,17 +6,17 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 async function movieExists(req, res, next) {
   const { movieId } = req.params;
   const movie = await service.read(movieId);
-  if( movie ) {
-      res.locals.movie = movie;
-      return next();
+  if (movie) {
+    res.locals.movie = movie;
+    return next();
   } else {
-      next({ status: 404, message: "Movie cannot be found." });
+    next({ status: 404, message: "Movie cannot be found." });
   }
 }
 
 async function list(req, res) {
   let movies;
-   // query for if movie is currently showing
+  // query for if movie is currently showing
   if (req.query.is_showing) {
     movies = await service.listMoviesCurrentlyShowing();
   } else {
@@ -25,12 +25,32 @@ async function list(req, res) {
   res.json({ data: movies });
 }
 
-function read(req, res) {
+async function read(req, res) {
   res.json({ data: res.locals.movie });
+}
+
+async function readTheaters(req, res) {
+  const { movieId } = req.params;
+  const data = await service.readTheaters(movieId);
+  res.json({ data });
+}
+
+async function readReviews(req, res) {
+  const { movieId } = req.params;
+  const data = await service.readReviews(movieId);
+  res.json({ data });
 }
 
 module.exports = {
   list: asyncErrorBoundary(list),
   movieExists: asyncErrorBoundary(movieExists),
   read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
+  readTheaters: [
+    asyncErrorBoundary(movieExists),
+    asyncErrorBoundary(readTheaters),
+  ],
+  readReviews: [
+    asyncErrorBoundary(movieExists),
+    asyncErrorBoundary(readReviews),
+  ],
 };
